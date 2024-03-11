6eddc1695580715b20f50aad66d2283d53f02559
b3LL3 <kyla.apolinario@student.ateneo.edu>
2024-03-11 14:52:36 +0800

    Modified lab2.

--- ledger/admin.py
+++ ledger/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from .models import *
 
 
+class RecipeIngredientAdmin(admin.TabularInline):
+    model = RecipeIngredient
+
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeIngredientAdmin, ]
 
 
 class IngredientAdmin(admin.ModelAdmin):
--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list', RecipesListView.as_view(), name='recipes_list'),
+    path('list/', RecipesListView.as_view(), name='recipes_list'),
     path('<int:pk>/detail', RecipeDetailsView.as_view(), name='recipe_detail'), ]
 
 app_name = "ledger"
